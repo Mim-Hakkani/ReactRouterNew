45d5a14dfa29ac2b59743adf7318e75523548e8e
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-04 17:24:46 +0600

    add loagin dynamic contion rendering and show the frind details information

--- src/components/FriendDetails/FriendDetails.js
+++ src/components/FriendDetails/FriendDetails.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
+   const [friendDetails,setFriendDetails] =useState({});
+   const [loading,setLoading]=useState(true)
 
     const {friendId} = useParams();
-    console.log('Friend id ',friendId);
+    // console.log('Friend id ',friendId);
+
+     useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users/${friendId}`)
+        .then(res=>res.json())
+        .then(data=>{
+            setFriendDetails(data);
+            setLoading(false);
+       })
+     },[]);
+
+     if(loading) return <p>loading please Wait ......</p>
     return (
         <div>
             <h1>Ami Friends er details to ki hoise </h1>
-            <h5>Amar id {friendId}</h5>
+            {!loading && <>
+             <h5>Amar id {friendId}</h5>
+            <p>Name is : {friendDetails?.name}</p>
+            <p>email:{friendDetails?.email}</p></>}
+           
         </div>
     );
 };
