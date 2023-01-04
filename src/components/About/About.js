9460fc5e30096a7eb3a7607d706a1ecf6700f1e3
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-04 15:33:13 +0600

    setup the react router and Routes and Route is used

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            This is About Section that i have seen 
+        </div>
+    );
+};
+
+export default About;
